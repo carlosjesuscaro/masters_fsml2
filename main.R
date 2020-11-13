44d8ad59b4770cae0d8ec76455241ef8169c0253
carloscaro <carlosjesuscaro@icloud.com>
2020-11-12 23:14:53 -0500

    First commit

--- main.R
+++ main.R
@@ -8,4 +8,8 @@ H = hist(A, breaks = c(0,0.2,0.4,0.6,0.8,1), freq = FALSE)
 H$counts
 max(A)
 min(A)
+# The area in the density is always equal to 1
+# The density histogram allows us to identify the family distribution
 
+# Loading datasets
+H1 <- hist(data1.txt, freq = FALSE)

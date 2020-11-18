fce56d4ce0f54ad322baf78bee2153caebe43349
carloscaro <carlosjesuscaro@icloud.com>
2020-11-17 19:59:42 -0500

    Class 2

--- main.R
+++ main.R
@@ -1,8 +1,10 @@
-# Title     : Class 1
+# Title     : Class 1 and 2
 # Objective : TODO
 # Created by: carloscaro
 # Created on: 2020-11-04
 
+# Class 1
+####################
 A = runif(1000)
 H = hist(A, breaks = c(0,0.2,0.4,0.6,0.8,1), freq = FALSE)
 H$counts
@@ -18,4 +20,15 @@ hist(B, freq = FALSE)
 # Creating a dataset with exponential distribution
 C = rexp(1000)
 hist(C, freq = FALSE)
+
+# In terminal, we can check and set the working directory with getwd() and setwd()
+
+# Class 2
+####################
+A = read.table('data1.txt')
+table(A)
+
+# From class problem
+x = seq(-1.5, 0.01)
+fx = 0*(x<0)+x^2/8*(x>=0)*(x<2)+(-x^3/8+x-1)*(x>=2)*(x<4)+1*(x>=4)
 
